9f42564934de19e511759a3887594c67707908e2
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-12-09 10:18:59 +0530

    Add files via upload

---
+++ Introduction to Data Science in Python/University of Michigan/Module 1/practice/Objects&Map.py
@@ -0,0 +1,20 @@
+class Person:
+    department = 'School of Information' #a class variable
+
+    def set_name(self, new_name): #a method
+        self.name = new_name
+    def set_location(self, new_location):
+        self.location = new_location
+
+person = Person()
+person.set_name('Christopher Brooks')
+person.set_location('Ann Arbor, MI, USA')
+print('{} live in {} and works in the department {}'.format(person.name, person.location, person.department))
+
+store1 = [10.00, 11.00, 12.34, 2.34]
+store2 = [9.00, 11.10, 12.34, 2.01]
+cheapest = map(min, store1, store2)
+print(cheapest)
+
+for item in cheapest:
+    print(item)
